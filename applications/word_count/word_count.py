72dfba7ce67e7ed8f19e7d374295ea84091e6b99
Sebastian Garces <gsgarces1@gmail.com>
2020-11-09 01:03:43 -0500

    day 3 and 4

--- applications/word_count/word_count.py
+++ applications/word_count/word_count.py
@@ -1,10 +1,28 @@
 def word_count(s):
     # Your code here
 
+    d = dict()
+
+    char_list = '"":;.,-+=/\|[]{}()*^&'
+
+    new_s = s.lower().replace("\t", " ").replace(
+        "\n", " ").replace("\r", " ").split(" ")
+
+    for word in new_s:
+        new_word = word.strip(char_list)
+
+        if new_word not in d and new_word != "":
+            d[new_word] = 1
+        elif new_word != "":
+            d[new_word] += 1
+
+    print(d)
+    return d
 
 
 if __name__ == "__main__":
     print(word_count(""))
     print(word_count("Hello"))
     print(word_count('Hello, my cat. And my cat doesn\'t say "hello" back.'))
-    print(word_count('This is a test of the emergency broadcast network. This is only a test.'))
+    print(word_count(
+        'This is a test of the emergency broadcast network. This is only a test.'))
